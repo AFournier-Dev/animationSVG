bdffa5136494a839675cd34d965150c724c5d180
AFournier-Dev <alexandre.fournier@yahoo.fr>
2020-11-20 15:59:40 +0100

    construction du projet allTBD WIP

---
+++ app.js
@@ -0,0 +1,48 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+/*
+canvas.width = 1200;
+canvas.height = 900;
+
+ctx.fillRect(12,120, 75, 75);
+ctx.fillRect(75, 75, 75, 75);
+ctx.fillRect(125, 125, 75, 75);
+ctx.fillRect(175, 175, 75, 75);
+ctx.fillRect(225, 225, 75, 75);
+ctx.fillRect(25, 25, 75, 75);
+ctx.fillRect(325, 325, 75, 75);
+ctx.fillRect(25, 25, 75, 75);
+
+
+ctx.beginPath();
+ctx.moveTo(75,50);
+ctx.lineTo(1100, 800);
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(600, 600, 600, 0, 2*Math.PI,);
+ctx.fillStyle = 'green';
+ctx.fill();
+ctx.stroke();
+
+
+ctx.fillStyle = 'black';
+ctx.fillRect(25, 25, 75, 75);
+ctx.fillRect(75, 75, 75, 75);
+ctx.fillRect(125, 125, 75, 75);
+ctx.fillRect(175, 175, 75, 75);
+ctx.fillRect(225, 225, 75, 75);
+ctx.fillRect(25, 25, 75, 75);
+ctx.fillRect(325, 325, 75, 75);
+ctx.fillRect(25, 25, 75, 75);
+
+
+ctx.beginPath();
+ctx.moveTo(75,50);
+ctx.lineTo(1100, 800);
+ctx.stroke();
+
+*/
